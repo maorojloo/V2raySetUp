ab5e1b65e51efb4026d9a62e92c34fd26d941f9d
Mohammad Amin Orojloo <ma.orojloo@gmail.com>
2022-11-04 22:49:22 +0330

    Add files via upload

---
+++ v2ray-upstream-server/config/user_gen.py
@@ -0,0 +1,69 @@
+import email
+import json
+import uuid
+import base64
+import socket
+
+
+
+
+with open('config.json', 'r') as openfile:
+    json_object = json.load(openfile)
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+server_ip=s.getsockname()[0]
+s.close()
+
+
+name=input("name=>")
+servername=input("servername=>")
+Port=input("Port=>")
+connection_type=input("connection type=>")
+
+
+email=name+"@"+servername+".meow"
+uuid=str(uuid.uuid4())
+
+new_User={"id": uuid, "alterId": 0, "security": "none", "email":email}
+json_object['inbounds'][0]['settings']['clients'].append(new_User)
+
+json_object=json.dumps(json_object, sort_keys=True, indent=4)
+
+#Writing to sample.json
+with open("config.json", "w") as outfile:
+    outfile.write(str(json_object))
+
+
+USER_JSON={
+  "add": server_ip,
+  "aid": "0",
+  "host": "",
+  "id": uuid,
+  "net": connection_type,
+  "path": "",
+  "port": Port,
+  "ps": name,
+  "sni": "",
+  "tls": "",
+  "type": "",
+  "v": "2"
+}
+
+
+sample_string = str(USER_JSON)
+sample_string_bytes = sample_string.encode("ascii")
+base64_bytes = base64.b64encode(sample_string_bytes)
+base64_string = base64_bytes.decode("ascii")
+
+
+
+
+uri="vmess://"+base64_string
+print("======================================================================================================")
+print()
+print(uri)
+print()
+print("======================================================================================================")
