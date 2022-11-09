0e9c230ab4b1c7b04d5fa79fa9e36cb5c38282c7
Mohammad Amin Orojloo <ma.orojloo@gmail.com>
2022-11-09 10:17:22 +0330

    Update user_gen.py

--- v2ray-bridge-server/config/user_gen.py
+++ v2ray-bridge-server/config/user_gen.py
@@ -4,12 +4,14 @@ import uuid
 import base64
 import socket
 import pyqrcode
+import random
+import string
 
 def get_random_string(length):
     # choose from all lowercase letter
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
-    print("Random string of length", length, "is:", result_str)
+    return result_str
 
 with open('config.json', 'r') as openfile:
     json_object = json.load(openfile)
