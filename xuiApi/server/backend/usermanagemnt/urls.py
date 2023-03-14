ac51d711a2fdc77e032e1af60b8217b08ba173ea
Mohammad Amin Orojloo <ma.orojloo@gmail.com>
2023-03-14 15:02:00 +0330

    Update urls.py

--- xuiApi/server/backend/usermanagemnt/urls.py
+++ xuiApi/server/backend/usermanagemnt/urls.py
@@ -7,6 +7,8 @@ from . import views
 urlpatterns = [
     path('telegram/geturi/<int:telId>',views.geturiTelegram,name='geturi'),
     path('telegram/addnewuser/<int:telId>/<int:userCount>/',views.addTelegramUseer,name='addTelegramUseer'),
+    path('telegram/deluri/<str:domain>/',views.deluri,name='deluri'),
+
     
     
 
